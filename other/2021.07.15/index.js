73a1278427174064c8c7e783a292ef9a461dda22
Wang Chunwei <wangchunv007@gmail.com>
2021-07-16 11:46:33 +0800

    feat：完善url字符解析对象算法

--- other/2021.07.15/index.js
+++ other/2021.07.15/index.js
@@ -18,9 +18,8 @@
 // 2、%20这种做了base64编码的，需要转换成字符串
 // 3、还有{}大括号的，要解析成对象
 // 4、就是针对给的4个case，4种处理，不是简单的字符串转json的，所以不能用eval
-
 function queryString(str) {
-  const strList = str.split('&');
+  const strList = decodeURI(str).split('&');
   const hashList = {};
 
   strList.forEach(item => {
@@ -39,9 +38,7 @@ function queryString(str) {
       }
     } 
     
-    if (!item.includes('=')) {
-      hashList[item] = false;
-    }
+    if (!item.includes('=')) hashList[item] = false;
   })
 
   return hashList;
@@ -49,19 +46,26 @@ function queryString(str) {
 
 // 只兼容了一层对象结构，还可以进行递归深度兼容
 function getObject(val) {
+  // todo 简版：适配多级对象
+  return JSON.parse(val.replace(/\w/g, v => `"${v}"`))
+
+  // todo 复杂版：适配多级对象时出现问题 
   // val = {a: 1, b: 2}
-  const str = val.replace(/[\{\}]/g, '');
-  console.log('大括号取出：', val, str);
+  // const str = val.replace(/[\{\}]/g, '');
+  // console.log('大括号取出：', val, str);
   
-  const strList = str.split(',');
-  const res = {};
-  strList.forEach(item => {
-    const [ key, value ] = item.split(':');
-    res[key] = value;
-  })
+  // const strList = str.split(',');
+  // const res = {};
+  // strList.forEach(item => {
+  //   const [ key, val ] = item.split(':');
 
-  return res;
+  //   if (/{.*}/.test(val)) {
+  //     res[key] = getObject(val);
+  //   } else {
+  //     res[key] = val;
+  //   }
+  // })
+  // return res;
 }
-
-const input = 'name=adam&name=bob&obj={a:1,b:2}&use&encodeStr=%20';
+const input = 'name=adam&name=bob&obj={a:1,b:2,c:{e:2}}&use&encodeStr=%20';
 console.log(queryString(input));

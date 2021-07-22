de030ba7a5f9b0d41fbb2a8de293bc5a28765d2b
Wang Chunwei <wangchunv007@gmail.com>
2021-07-22 11:21:50 +0800

    feat：新增相同的二叉树算法

---
+++ leetcode/100. 相同的树/index.js
@@ -0,0 +1,22 @@
+/**
+ * 给两个二叉树，如何确定两个二叉树完全相等
+ * https://leetcode-cn.com/problems/same-tree/
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+  if (p === null && q === null) return true;
+  if (p === null || q === null) return false;
+  if (p.val !== q.val) return false;
+
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
